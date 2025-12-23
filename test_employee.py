898362964f457860124bb88c118987de6316f84f
Nagaraj Kuntoji <kn733478@gmail.com>
2025-12-23 12:30:08 +0530

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -1,9 +1,9 @@
 import pytest
 from Employee import Employee_details
-def test_employee.py():
+def test_employee_details.py():
    expected_output=(
   empname="Harsha"
   empid="S109"
   salary=70000
 )
-assert(Employee_details("Harsha","S109",70000)==expected_output
+assert Employee_details("Harsha","S109",70000)==expected_output
